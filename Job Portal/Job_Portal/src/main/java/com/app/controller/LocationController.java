fc0751ce5909eb00cab2f1f8c5a659849863d156
Komalrochlani <131248534+Komalrochlani@users.noreply.github.com>
2023-08-26 17:07:10 +0530

    locationinsert

--- Job Portal/Job_Portal/src/main/java/com/app/controller/LocationController.java
+++ Job Portal/Job_Portal/src/main/java/com/app/controller/LocationController.java
@@ -5,13 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.dto.LocationDto;
 import com.app.entity.Location;
 import com.app.service.LocationService;
+
+
 
 @RestController
 @RequestMapping("/location")
@@ -21,12 +25,16 @@ public class LocationController {
 	private LocationService locationService;
 	
 	@PostMapping
-	public ResponseEntity<?> saveLocationDetails(@RequestBody Location location)
+	public ResponseEntity<?> saveLocationDetails(@RequestBody LocationDto location)
 	{
 		String result = locationService.insertLocationDetails(location);
 		return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
 	
-	
-	
+	@GetMapping("/list")
+	public List<Location> listAllLocation(){
+		
+		
+		return locationService.allLocation();
+	}
 }
